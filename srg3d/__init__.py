c7a4f98545b61c20edb6d4547111fb4fa23dec29
Matthias Heinz <heinz.matthias.3@gmail.com>
2018-11-14 08:58:30 +0100

    srg3d - Add docstring to srg3d package __init__

--- srg3d/__init__.py
+++ srg3d/__init__.py
@@ -1,3 +1,12 @@
+"""Library for the easy evolution of momentum-space potentials in 3D.
+
+Provides:
+    1. An SRG object to evolve potentials
+    2. Classes for single channel and coupled channel potentials
+    3. Interface to read potentials from files and save them in standard
+        directories
+
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
